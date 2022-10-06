2e4361eb8527c68ca95b765b908ffb88d21619df
Edwar <ejaimesp@ufpso.edu.co>
2022-10-06 18:45:05 -0500

    Commit init

---
+++ src/main/java/com/Parcial/Procesos/ParcialProcesos/ParcialProcesosApplication.java
@@ -0,0 +1,13 @@
+package com.Parcial.Procesos.ParcialProcesos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialProcesosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialProcesosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Parcial/Procesos/ParcialProcesos/ParcialProcesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Parcial.Procesos.ParcialProcesos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialProcesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
